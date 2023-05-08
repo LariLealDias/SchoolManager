7251fcc926fcd84c46746ebba535f9d5a7b2f160
Lari <larileal@hotmail.com.br>
2023-05-08 17:17:59 -0300

    Feat: adding Model to Subject entity

---
+++ GerenciadorEscolar/Models/SubjectModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GerenciadorEscolar.Models;
+
+public class SubjectModel
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Fild Title is required")]
+    [StringLength(30, ErrorMessage = "Fild Title could only aceppt till 50 characters")]
+    public string Title { get; set; }
+}
