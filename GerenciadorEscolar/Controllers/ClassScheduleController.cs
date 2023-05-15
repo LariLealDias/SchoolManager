c5b612597022d308b6deebe6fed66cf78bf5f2ff
Lari <larileal@hotmail.com.br>
2023-05-15 11:24:58 -0300

    Feat: adding documentation to ClassSchedule Controller

--- GerenciadorEscolar/Controllers/ClassScheduleController.cs
+++ GerenciadorEscolar/Controllers/ClassScheduleController.cs
@@ -19,6 +19,15 @@ public class ClassScheduleController : ControllerBase
         _contex = contex;
     }
 
+    #region ----------------------   Ducumentação API ---------------------
+    /// <summary>
+    /// Criar uma Aula ao banco de dados e retornar informações no Header
+    /// </summary>
+    /// <param name="classScheduleDto">Objeto com os campos necessários para criação de um Aula</param>
+    /// <returns> retornar seus dados com Location no Header </returns>
+    /// <response code="201">Caso inserção seja feita com sucesso</response>
+    /// <response code="400">Erro ao criar o Turma</response>
+    #endregion
     [HttpPost]
     public IActionResult CreateClassSchedule([FromBody] CreateClassScheduleDto classScheduleDto)
     {
@@ -40,14 +49,39 @@ public class ClassScheduleController : ControllerBase
         }
     }
 
+
+
+    #region ----------------------   Ducumentação API ---------------------
+    /// <summary>
+    /// Retorna uma lista de Aulas do banco de dados conforme a paginação
+    /// </summary>
+    /// <param name="skip">primeiro dado para a paginação </param>
+    /// <param name="take">ultimo dado para a paginação</param>
+    /// <returns> lista mapeada com todas as instancias </returns>
+    /// <response code="200">Caso o retorno seja feito com sucesso</response>
+    #endregion
     [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     public IEnumerable<ReadClassScheduleDto> GetClassSchedule([FromQuery] int skip = 0,
                                                               [FromQuery] int take = 10)
     {
         return _mapper.Map<List<ReadClassScheduleDto>>(_contex.ClassSchedules.Skip(skip).Take(take).ToList());
     }
 
+
+    #region ----------------------   Ducumentação API ---------------------
+    /// <summary>
+    /// Retorna 1 objeto de Aula do banco de dados conforme o Id do Professor e o Id da Turma 
+    /// </summary>
+    /// <param name="teacherModelId"> dado para encontra o Professor relacionado a Aula no banco de dados </param>
+    /// <param name="classModelId"> dado para encontra a Turma relacionada a Aula no banco de dados </param>
+    /// <returns> dado de uma única Aula </returns>
+    /// <response code="200">Caso o retorno seja feito com sucesso</response>
+    /// <response code="400">Erro ao criar a Turma</response>
+    #endregion
     [HttpGet("{TeacherModelId}/{ClassModelId}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public IActionResult GetClassScheduleByTwoIds(int teacherModelId, int classModelId)
     {
         try
